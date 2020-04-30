06b8349cf4e2f91e29861beecc854d023a75776b
TaniaAlvarezDiaz <tani.alvarez.diaz@gmail.com>
2020-04-30 18:10:58 +0200

    creada estructura del proyecto

---
+++ app/src/main/java/com/miw/dsdm/miwlibrary/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.miw.dsdm.miwlibrary.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miw.dsdm.miwlibrary.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
